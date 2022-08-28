eaabff5b7f5c100cfa25bd5f03e1066faa4483a1
luxcgo <luxcgo@gmail.com>
2022-08-29 01:26:33 +0800

    init

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,10 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import ElementPlus from "element-plus";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(ElementPlus);
 
 app.mount("#app");
 
