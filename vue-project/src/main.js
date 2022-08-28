8383cc49ef03814653c40ff12fbdf4f8ca630bbc
luxcgo <luxcgo@gmail.com>
2022-08-28 22:25:22 +0800

    init

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import "./assets/main.css";
 
@@ -9,3 +10,8 @@ const app = createApp(App);
 app.use(router);
 
 app.mount("#app");
+
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
---
+++ vue-project/src/views/login/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password" type="text"></el-input>
+        <span class="show-pwd">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+
+      <el-button
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+      ></el-button>
+    </el-form>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
